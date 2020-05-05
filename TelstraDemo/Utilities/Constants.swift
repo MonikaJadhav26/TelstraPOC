6361301093dfe9ec661b40ac61b3173d3c39fed9
Monika Jdhav <mj00565663@techmahindra.com>
2020-05-05 12:56:03 +0530

    Created basic folder structure and worked on tableview UI.

---
+++ TelstraDemo/Utilities/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  UITablviewPOC
+//
+//  Created by Monika Jadhav on 20/04/20.
+//  Copyright © 2020 rp00612313. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let kCellIdentifier = "FactsTableViewCell"
+let kTopBottomRightLeftPaddingConstant  =  10
+let imageViewHeightWidthConstant = 60
+let labelHeightConstant = 20
+let boldFontConstant = 15
+let regularFontConstant = 12
+let defaultImage = UIImage(named: "default")
---
+++ TelstraDemo/Utilities/UIImageView+download.swift
@@ -0,0 +1,41 @@
+//
+//  UIImageView+download.swift
+//  UITablviewPOC
+//
+//  Created by Monika Jadhav on 20/04/20.
+//  Copyright © 2020 rp00612313. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+   
+    func downloaded(from link: String) {
+        let imageCache = NSCache<NSString, UIImage>()
+        self.image = defaultImage
+        let imageServerUrl = link.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        if let cachedImage = imageCache.object(forKey: NSString(string: imageServerUrl)) {
+            self.image = cachedImage
+            return
+        }
+        if let url = URL(string: imageServerUrl) {
+            URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                if error != nil {
+                    DispatchQueue.main.async {
+                        self.image = defaultImage
+                    }
+                    return
+                }
+                DispatchQueue.main.async {
+                    if let data = data {
+                        if let downloadedImage = UIImage(data: data) {
+                            imageCache.setObject(downloadedImage, forKey: NSString(string: imageServerUrl))
+                            self.image = downloadedImage
+                        }
+                    }
+                }
+            }).resume()
+        }
+    }
+}
